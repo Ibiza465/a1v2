79024a8bb74bfd9cc92a6e4a7a288cb770d29bd2
Shankar Thapa <shankarthapa465@gmail.com>
2022-10-05 23:31:44 -0400

    initial commit

---
+++ test.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+    res.send("Blog Page<br /><a href='/Blog'>Go to the Blog page</a>");
+});
+
+
+app.get("/blog", function(req,res){
+    res.sendFile(path.join(__dirname,"/blog.html"));
+  });
+
+  app.get("/login", function(req,res){
+    res.sendFile(path.join(__dirname,"/login.html"));
+  });
+
+  app.get("/article", function(req,res){
+    res.sendFile(path.join(__dirname,"/article.html"));
+  });
+  app.get("/register", function(req,res){
+    res.sendFile(path.join(__dirname,"/register.html"));
+  });
+
+
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
